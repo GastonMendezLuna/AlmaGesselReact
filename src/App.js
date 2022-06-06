3b564ab8e6ed32cea386a0510ed9e68b30015e70
gaston mendez luna <mendezlunagaston@gmail.com>
2022-06-06 16:44:10 -0300

    Terminado NavBar entrega 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
    
     <div>
-      <div className="App"> Mi primer carrito</div>;
       <NavBar/>;
     </div>
     
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,13 @@ import "./navbar.css";
 export default function NavBar() {
   
   return (
-    <nav className="nav-main">
-        <ul>
+    <nav className='nav-main'>
+        <ul className='nav-justify' >
             <img className='logo-nav' src={logoAlma} alt="logo"/>
-            <li>Alma Gesell</li>
-            <li>Sobre Nosotros</li>
-            <li>Productos</li>
-            <li>Carrito</li>
+            <li className='link-nav' >Alma Gesell</li>
+            <li className='link-nav'>Sobre Nosotros</li>
+            <li className='link-nav'>Productos</li>
+            <li className='link-nav'>Carrito</li>
         </ul>
     </nav>
 
